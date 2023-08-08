e2f57c49599fb8888b076a0238bf773a3179ac54
ScheersKarel <karel.scheers@student.kdg.be>
2023-08-08 13:25:38 +0200

    taal aanpassing

--- loginCheck.php
+++ loginCheck.php
@@ -40,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             } 
             
             else {
-                $_SESSION["emailError"] = "email and/or wachtwoord are incorrect";
+                $_SESSION["emailError"] = "email of wachtwoord zijn incorrect";
                 header("location: login.php");
             }
         } 
